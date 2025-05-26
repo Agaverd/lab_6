c94c77349aa24d8b3749d9b20488ec9c9cc99f6d
Agaverd <agaverd.seyfaev@gmail.com>
2025-05-26 17:13:21 +0300

    add: realization of sorting startegy

--- src/lab_6.cpp
+++ src/lab_6.cpp
@@ -1,63 +1,77 @@
-#pragma once
-
-#ifndef LAB_6_LIB_HPP
-#define LAB_6_LIB_HPP
-
-#include <vector>
-#include <algorithm>
+#include <lab6.hpp>
 
 namespace lab
 {
-    // SortingStrategy
-    class SortingStrategy
-    {
-    public:
-        virtual void sort(std::vector<int>& data) const = 0;
-        virtual ~SortingStrategy() = default;
-    };
-
     // BubbleSort
-    class BubbleSortStrategy : public SortingStrategy
+    void BubbleSortStrategy::sort(std::vector<int>& data) const
     {
-    public:
-        void sort(std::vector<int>& data) const override;
-    };
+        int n = data.size();
+        for (int i = 0; i < n - 1; ++i)
+        {
+            for (int j = 0; j < n - i - 1; ++j)
+            {
+                if (data[j] > data[j + 1]) std::swap(data[j], data[j + 1]);
+            }
+        }
+    }
 
     // QuickSort
-    class QuickSortStrategy : public SortingStrategy
+    int QuickSortStrategy::partition(std::vector<int>& data, int low, int high) const
     {
-    public:
-        void sort(std::vector<int>& data) const override;
+        int pivot = data[high];
+        int i = low - 1;
 
-    private:
-        int partition(std::vector<int>& data, int low, int high) const;
-        void quickSort(std::vector<int>& data, int low, int high) const;
-    };
+        for (int j = low; j < high; ++j)
+        {
+            if (data[j] <= pivot)
+            {
+                ++i;
+                std::swap(data[i], data[j]);
+            }
+        }
+        std::swap(data[i + 1], data[high]);
+
+        return i + 1;
+    }
+
+    void QuickSortStrategy::quickSort(std::vector<int>& data, int low, int high) const
+    {
+        if (low < high)
+        {
+            int pi = partition(data, low, high);
+            quickSort(data, low, pi - 1);
+            quickSort(data, pi + 1, high);
+        }
+    }
+
+    void QuickSortStrategy::sort(std::vector<int>& data) const
+    {
+        if (!data.empty()) quickSort(data, 0, data.size() - 1);
+    }
 
     // StdSort
-    class StdSortStrategy : public SortingStrategy
+    void StdSortStrategy::sort(std::vector<int>& data) const
     {
-    public:
-        void sort(std::vector<int>& data) const override;
-    };
+        std::sort(data.begin(), data.end());
+    }
 
     // InsertionSort
-    class InsertionSortStrategy : public SortingStrategy
+    void InsertionSortStrategy::sort(std::vector<int>& data) const
     {
-    public:
-        void sort(std::vector<int>& data) const override;
-    };
+        for (size_t i = 1; i < data.size(); ++i)
+        {
+            int key = data[i];
+            int j = i - 1;
+            while (j >= 0 && data[j] > key)
+            {
+                data[j + 1] = data[j];
+                --j;
+            }
+            data[j + 1] = key;
+        }
+    }
 
     // Sorter
-    class Sorter
-    {
-    public:
-        Sorter(const SortingStrategy* strategy);
-        void sortData(std::vector<int>& data) const;
-
-    private:
-        const SortingStrategy* strategy_;
-    };
+    Sorter::Sorter(const SortingStrategy* strategy) : strategy_(strategy) {}
+    void Sorter::sortData(std::vector<int>& data) const { strategy_->sort(data); }
 }
-
-#endif
